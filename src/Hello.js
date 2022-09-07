aaa9d151368beccd71ca0ea58f826e10e6c82869
Balogun-Damilare <balogundamilaree@gmail.com>
2022-09-07 16:46:49 +0100

    new commit

--- src/Hello.js
+++ src/Hello.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import './Hello.css'
+import React from "react";
+import "./Hello.css";
 
 class Hello extends React.Component {
-	render() {
-		return <div class='f1 tc'>
-			<h1> Hello World </h1>
-		<p>{this.props.greeting} </p>
-		</div>
-		
-	}
+  render() {
+    return (
+      <div class="f1 tc">
+        <h1> Hello Samson this is the changes made </h1>
+        <p>{this.props.greeting} </p>
+      </div>
+    );
+  }
 }
 
-export default Hello
+export default Hello;
